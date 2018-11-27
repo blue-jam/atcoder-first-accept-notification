19244609b716bf489b3abaf7692b136710099a74
blue-jam <blue-jam@users.noreply.github.com>
2018-11-27 21:49:24 +0900

    Add mock frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">AtCoder First Accept Notification</h1>
+                </header>
+                <div>
+                    <input type="text" name="contestId"/>
+                    <button type="button">watch</button>
+                    <button type="button">stop</button>
+                    <button type="button">reset</button>
+                </div>
+                <div>
+                    <table>
+                        <thead><tr><th>Problem</th><th>Accepted</th></tr></thead>
+                        <tbody>
+                            <tr><td>A</td><td>-</td></tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
